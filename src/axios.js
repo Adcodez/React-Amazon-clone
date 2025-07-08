ba85d1b16846bc3ec1f11c514c0dbc8a48570018
Adam Kupi <adamkupi689@gmail.com>
2025-07-08 15:58:01 +0200

    fixed stripe issue again

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://us-central1-clone-21ff4.cloudfunctions.net/api"
-      : "http://localhost:5001/clone-21ff4/us-central1/api",
+  baseURL: "http://localhost:5001/clone-21ff4/us-central1/api",
 });
-
 
 export default instance;
